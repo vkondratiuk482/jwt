45f3d9d6f653c3755352a2e9b8b05a5d6fed9788
mokuteki <kondratiuk011@gmail.com>
2023-02-08 23:30:31 +0200

    feat(tests): hs256 strategy corner cases test coverage

--- test/jwt.js
+++ test/jwt.js
@@ -4,44 +4,114 @@ const { Jwt } = require('../src/jwt.js');
 const { HS256Strategy } = require('../src/strategies/hs256.js');
 const { RS256Strategy } = require('../src/strategies/rs256.js');
 const { InvalidHeaderError } = require('../src/errors/invalid-header-error.js');
+const { InvalidSignatureError } = require('../src/errors/invalid-signature-error.js');
+const { JwtExpiredError } = require('../src/errors/jwt-expired-error.js');
 
+const secret = 'ANY_SECRET';
+const payload = { id: 1 };
+const invalid_header_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVDEifQ.eyJpZCI6IjEyMyIsImV4cCI6IjExNjcyNjc2NzQxNjM0In0.67iDd-sQQ-vIV6xJNxd7jfw49COMVpTdlsDO1B7D4l0';
+const invalid_signature_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEyMyIsImV4cCI6IjExNjcyNjc2NzQxNjM0In0.7s0MhJSiWSsyOsNRiWjgJTLk9Y_Fz6tY4FcwFpDkfQY';
 
 describe('HS256Strategy', () => {
-  it('TypeError', () => {
-    assert.throws(() => {
-      const ttl = 'ttl';
-      const secret = 'ANY_SECRET';
+  it('Successfully create and verify token', () => {
+    const jwt = new Jwt(new HS256Strategy({
+      secret,
+      ttl: 10000,
+    }));
 
-      const jwt = new Jwt(new HS256Strategy({
-        ttl,
+    const token = jwt.generate(payload);
+    const decoded = jwt.verify(token);
+
+    assert.deepStrictEqual(decoded, payload);
+  });
+
+  it('Invalid ttl type', () => {
+    const handler = () => {
+      new HS256Strategy({
         secret,
-      }));
-    }, TypeError);
+        ttl: 'ttl',
+      });
+    };
 
-    assert.throws(() => {
-      const ttl = -1;
-      const secret = 'ANY_SECRET';
+    assert.throws(handler, TypeError);
+  });
 
-      const jwt = new Jwt(new HS256Strategy({
-        ttl,
+  it('Invalid ttl value', () => {
+    const handler = () => {
+      new HS256Strategy({
         secret,
-      }));
-    }, TypeError);
+        ttl: -1,
+      });
+    };
+
+    assert.throws(handler, TypeError);
   });
 
   it('InvalidHeaderError', () => {
-    assert.throws(() => {
-      const ttl = 10000;
-      const secret = 'ANY_SECRET';
-
+    const handler = () => {
       const jwt = new Jwt(new HS256Strategy({
-        ttl,
         secret,
+        ttl: 10000,
       }));
 
-      const invalidHeaderJwt = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVDEifQ.eyJpZCI6IjEyMyIsImV4cCI6IjExNjcyNjc2NzQxNjM0In0.67iDd-sQQ-vIV6xJNxd7jfw49COMVpTdlsDO1B7D4l0';
+      jwt.verify(invalid_header_token);
+    };
 
-      jwt.verify(invalidHeaderJwt);
-    }, InvalidHeaderError);
+    assert.throws(handler, InvalidHeaderError);
+  });
+
+  it('InvalidSignatureError', () => {
+    const handler = () => {
+      const jwt = new Jwt(new HS256Strategy({
+        secret,
+        ttl: 10000,
+      }));
+
+      jwt.verify(invalid_signature_token);
+    };
+
+    assert.throws(handler, InvalidSignatureError);
+  });
+
+  it('JwtExpiredError with options passed in constructor', (done) => {
+    const jwt = new Jwt(new HS256Strategy({
+      secret,
+      ttl: 1000,
+    }));
+
+    const token = jwt.generate(payload);
+    const decoded = jwt.verify(token);
+
+    assert.deepStrictEqual(decoded, payload);
+
+    setTimeout(() => {
+      try {
+        jwt.verify(token)
+      } catch (err) {
+        assert(err instanceof JwtExpiredError);
+        done();
+      }
+    }, 2000);
+  });
+
+  it('Prevent JwtExpiredError by overriding options in "generate" method', (done) => {
+    const jwt = new Jwt(new HS256Strategy({
+      secret,
+      ttl: 1000,
+    }));
+
+    const token = jwt.generate(payload, { ttl: 3000 });
+    const decoded = jwt.verify(token);
+
+    assert.deepStrictEqual(decoded, payload);
+
+    setTimeout(() => {
+      try {
+        jwt.verify(token)
+        done();
+      } catch (err) {
+        // won't happen
+      }
+    }, 2000);
   });
 });
