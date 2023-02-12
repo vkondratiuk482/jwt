214d0ae25e8a91b64159dffef945a09495eea6aa
mokuteki <kondratiuk011@gmail.com>
2023-02-12 21:36:24 +0200

    feat(app): typescript types for the application

--- jwt.js
+++ jwt.js
@@ -5,7 +5,9 @@ const { HS256Strategy } = require('./lib/strategies/hs256.js');
 const { RS256Strategy } = require('./lib/strategies/rs256.js');
 const { JwtExpiredError } = require('./lib/errors/jwt-expired-error.js');
 const { InvalidHeaderError } = require('./lib/errors/invalid-header-error.js');
-const { InvalidSignatureError } = require('./lib/errors/invalid-signature-error.js');
+const {
+  InvalidSignatureError,
+} = require('./lib/errors/invalid-signature-error.js');
 
 module.exports.JSONWebToken = JSONWebToken;
 module.exports.HS256Strategy = HS256Strategy;
---
+++ types/jwt.d.ts
@@ -0,0 +1,63 @@
+export type JSONWebTokenOptions = HS256StrategyOptions | RS256StrategyOptions;
+
+export interface JSONWebTokenHeader {
+  readonly typ: string;
+
+  readonly alg: string;
+}
+
+export interface BaseStrategyOptions {
+  readonly header: JSONWebTokenHeader;
+
+  readonly ttl: number;
+}
+
+export abstract class BaseStrategy {
+  abstract sign(unsigned: string): string;
+
+  abstract validateSignature(unsigned: string, candidate: string): boolean;
+
+  generate(payload: object, options?: JSONWebTokenOptions): string;
+
+  verify<T>(token: string): T;
+}
+
+export interface HS256StrategyOptions {
+  readonly ttl: number;
+
+  readonly secret: string;
+}
+
+export interface RS256StrategyOptions {
+  readonly ttl: number;
+
+  readonly publicKey: string;
+
+  readonly privKey: string;
+}
+
+export class HS256Strategy extends BaseStrategy {
+  constructor(options: HS256StrategyOptions);
+
+  sign(unsigned: string): string;
+
+  validateSignature(unsigned: string, candidate: string): boolean;
+}
+
+export class RS256Strategy extends BaseStrategy {
+  constructor(options: RS256StrategyOptions);
+
+  sign(unsigned: string): string;
+
+  validateSignature(unsigned: string, candidate: string): boolean;
+}
+
+export class JSONWebToken {
+  constructor(strategy: BaseStrategy);
+
+  setStrategy(strategy: BaseStrategy): void;
+
+  generate(payload: object, options?: JSONWebTokenOptions): string;
+
+  verify<T>(token: string): T;
+}
