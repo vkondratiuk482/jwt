85fde82d9d6ab2d87933a88e610582551e6ec96f
mokuteki <kondratiuk011@gmail.com>
2023-02-18 22:08:34 +0200

    chore(jwt): get rid of extra file content

--- jwt.js
+++ jwt.js
@@ -9,22 +9,6 @@ const {
   InvalidSignatureError,
 } = require('./lib/errors/invalid-signature-error.js');
 
-const strategy = new HS256Strategy({
-  ttl: 10000,
-  secret: 'YOUR_SECRET',
-});
-
-const jwt = new JSONWebToken(strategy);
-const payload = { id: 1 };
-
-const token = jwt.generate(payload, { ttl: 20000, secret: 'NEW_SECRET' });
-
-try {
-  const decoded = jwt.verify(token);
-} catch (err) {
-  // err instanceof InvalidSignatureError
-}
-
 module.exports.JSONWebToken = JSONWebToken;
 module.exports.HS256Strategy = HS256Strategy;
 module.exports.RS256Strategy = RS256Strategy;
